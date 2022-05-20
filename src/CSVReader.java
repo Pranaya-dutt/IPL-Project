d7c00c4fbe94409bcfa5bb1caba273948a3125a1
Pranaya Dutt <pranayadutt2000@gmail.com>
2022-05-21 02:48:41 +0530

    2nd Scenario Done

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 import static java.lang.Integer.parseInt;
 
@@ -14,6 +15,8 @@ public class CSVReader {
         for(int i=0;i<10;i++){
             years[i] = 0;
         }
+        HashMap<String,Integer> wonMatches = new HashMap<String, Integer>();
+        int winCount = 0;
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(MatchesPath));
@@ -28,6 +31,16 @@ public class CSVReader {
                 //System.out.println(values[1]);
                 int index = parseInt(values[1])-2008;
                 years[index]++;
+
+                if(!wonMatches.containsKey(values[10])){
+                    winCount = 1;
+                    wonMatches.put(values[10], winCount);
+                } else{
+                    int count = wonMatches.get(values[10]);
+                    count++;
+                    wonMatches.put(values[10], count);
+                    count=0;
+                }
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -41,6 +54,12 @@ public class CSVReader {
             System.out.println("Matches played in " + (i+2008)+ " = " + years[i]);
         }
         System.out.println("----------------------");
+
+        System.out.println();
+
+        System.out.println("------Scenario 2------");
+        System.out.println(wonMatches);
+
 
 
     }
